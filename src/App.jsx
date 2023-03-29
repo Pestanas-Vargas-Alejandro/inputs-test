14be32fcaca5e7bf4e6f9137bbaa2d0ac85cba46
Pestanas-Vargas-Alejandro <isc19350316@gmail.com>
2023-03-28 18:49:24 -0600

    Creacion diversos tipos de inputs con variables de estado v2

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,25 @@ function App() {
   const [inputValue, setInputValue] = useState({
     input1: "",
     input2: "",
-    password: "",
-    checkbox: "",
-    color: "",
     date: "",
-    datetime: "",
     email: "",
+    password: "",
+    tel: "",
+    color: "",
+    number: "",
+    range: "",
+    checkbox: "",
+    datetime: "",
+    month: "",
+    time: "",
+    week: "",
+    reset: "",
+    file: "",
     hidden: "",
     image: "",
-    month: "",
-    number: "",
     radio: "",
-    range: "",
-    reset: "",
     search: "",
+    url: "",
   });
   const handleInputChange = (e) => {
     setInputValue({
@@ -28,174 +33,264 @@ function App() {
   }
 
   return (
-    <div className="inputs mt-5">
-      <h1>Input Test</h1>
-      <h4>Text</h4>
-      <div className="input-text">
-        <label htmlFor="input1">Introduce Valor 1 : </label>
-        <input
-          id="input1"
-          name="input1"
-          onChange={(e) => handleInputChange(e)}
-          type="text"
-          placeholder="e.j. Arturo"
-          value={inputValue.input1}
-        />
-        <br /><br />
-        <label>
-          Introduce Valor 2 :
+    <div className="container text-center mt-5">
+      <h1>Formulario (Inputs tests)</h1>
+      <div className="inputs-row1 row mt-5">
+        <div className="col">
+          <label htmlFor="input1"> Nombre : </label>
           <input
-            name="input2"
+            className="form-control mt-2"
+            id="input1"
+            name="input1"
             onChange={(e) => handleInputChange(e)}
             type="text"
-            placeholder="e.j. Lopez Gonzalez"
-            value={inputValue.input2}
+            value={inputValue.input1}
           />
-        </label>
+        </div>
+
+        <div className="col">
+          <label >
+            Apellidos :
+            <input
+              className="form-control mt-2"
+              name="input2"
+              onChange={(e) => handleInputChange(e)}
+              type="text"
+              value={inputValue.input2}
+            />
+          </label>
+        </div>
+
+        <div className="col">
+          <label htmlFor="date"> Fecha Nacimiento : </label>
+          <input
+            className="form-control mt-2"
+            id="date"
+            name="date"
+            onChange={(e) => handleInputChange(e)}
+            type="date"
+            value={inputValue.date}
+          />
+        </div>
+
+        <div className="col">
+          <label htmlFor="email"> Email : </label>
+          <input
+            className="form-control mt-2"
+            id="email"
+            name="email"
+            onChange={(e) => handleInputChange(e)}
+            type="email"
+            value={inputValue.email}
+          />
+        </div>
+
+        <div className="col">
+          <label htmlFor="password"> Contraseña : </label>
+          <input
+            className="form-control mt-2"
+            id="password"
+            name="password"
+            onChange={(e) => handleInputChange(e)}
+            type="password"
+            value={inputValue.password}
+          />
+        </div>
       </div>
 
-      <div className="others mt-2">
-        <h4>Password</h4>
-        <label htmlFor="password">Contraseña : </label>
-        <input
-          id="password"
-          name="password"
-          onChange={(e) => handleInputChange(e)}
-          type="password"
-          value={inputValue.password}
-        />
+      <div className="inputs-row2 row mt-5">
+        <div className="col">
+          <label htmlFor="tel"> Contacto : </label>
+          <input
+            className="form-control mt-2"
+            id="tel"
+            name="tel"
+            onChange={(e) => handleInputChange(e)}
+            type="tel"
+            value={inputValue.tel}
+          />
+        </div>
 
-        <h4>Checkbox</h4>
-        <label htmlFor="password">Acepto Terminos y Condiciones </label>
-        <input
-          id="checkbox"
-          name="checkbox"
-          onChange={(e) => handleInputChange(e)}
-          type="checkbox"
-          value={inputValue.checkbox}
-        />
+        <div className="col">
+          <label htmlFor="color"> Color : </label>
+          <input
+            className="form-control mt-2"
+            id="color"
+            name="color"
+            onChange={(e) => handleInputChange(e)}
+            type="color"
+            value={inputValue.color}
+          />
+        </div>
 
-        <h4>Color</h4>
-        <label htmlFor="color">Seleccione un color : </label>
-        <input
-          id="color"
-          name="color"
-          onChange={(e) => handleInputChange(e)}
-          type="color"
-          value={inputValue.color}
-        />
+        <div className="col">
+          <label htmlFor="number"> Cantidad : </label>
+          <input
+            className="form-control mt-2"
+            id="number"
+            name="number"
+            onChange={(e) => handleInputChange(e)}
+            type="number"
+            value={inputValue.number}
+          />
+        </div>
 
-        <h4>Date</h4>
-        <label htmlFor="color">Ingrese Fecha Nacimiento: </label>
-        <input
-          id="date"
-          name="date"
-          onChange={(e) => handleInputChange(e)}
-          type="date"
-          value={inputValue.date}
-        />
+        <div className="col">
+          <label htmlFor="range"> Rango de precio : </label>
+          <input
+            id="range"
+            name="range"
+            onChange={(e) => handleInputChange(e)}
+            type="range"
+            value={inputValue.range}
+          />
+        </div>
 
-        <h4>Datetime-local</h4>
-        <label htmlFor="color">Ingrese Fecha y Hora: </label>
-        <input
-          id="datetime"
-          name="datetime"
-          onChange={(e) => handleInputChange(e)}
-          type="datetime-local"
-          value={inputValue.datetime}
-        />
-
-        <h4>Email</h4>
-        <label htmlFor="email">Ingrese Email: </label>
-        <input
-          id="email"
-          name="email"
-          onChange={(e) => handleInputChange(e)}
-          type="email"
-          value={inputValue.email}
-        />
-
-        <h4>Hidden</h4>
-        <label htmlFor="hidden">Ingrese id: </label>
-        <input
-          id="hidden"
-          name="hidden"
-          onChange={(e) => handleInputChange(e)}
-          type="hidden"
-          value={inputValue.hidden}
-        />
-
-        <h4>Image</h4>
-        <label htmlFor="image">Ingrese imagen: </label>
-        <input
-          id="image"
-          name="image"
-          onChange={(e) => handleInputChange(e)}
-          type="image"
-          value={inputValue.image}
-        />
-
-        <h4>Month</h4>
-        <label htmlFor="month">Ingrese mes: </label>
-        <input
-          id="month"
-          name="month"
-          onChange={(e) => handleInputChange(e)}
-          type="month"
-          value={inputValue.month}
-        />
-
-        <h4>Number</h4>
-        <label htmlFor="number">Cantidad: </label>
-        <input
-          id="number"
-          name="number"
-          onChange={(e) => handleInputChange(e)}
-          type="number"
-          value={inputValue.number}
-        />
-
-        <h4>Radio</h4>
-        <label htmlFor="radio">Agregar: </label>
-        <input
-          id="radio"
-          name="radio"
-          onChange={(e) => handleInputChange(e)}
-          type="radio"
-          value={inputValue.radio}
-        />
-
-        <h4>Range</h4>
-        <label htmlFor="range">Precio: </label>
-        <input
-          id="range"
-          name="range"
-          onChange={(e) => handleInputChange(e)}
-          type="range"
-          value={inputValue.range}
-        />
-
-        <h4>Reset</h4>
-        <label htmlFor="reset">Borrar Selección: </label>
-        <input
-          id="reset"
-          name="reset"
-          onChange={(e) => handleInputChange(e)}
-          type="reset"
-          value={inputValue.reset}
-        />
-
-        <h4>Search</h4>
-        <label htmlFor="search">Buscar: </label>
-        <input
-          id="search"
-          name="search"
-          onChange={(e) => handleInputChange(e)}
-          type="search"
-          value={inputValue.search}
-        />
-
+        <div className="col">
+          <label htmlFor="checkbox"> Envio express : </label>
+          <input
+            id="checkbox"
+            name="checkbox"
+            onChange={(e) => handleInputChange(e)}
+            type="checkbox"
+            value={inputValue.checkbox}
+          />
+        </div>
       </div>
+
+      <div className="inputs-row3 row mt-5">
+        <div className="col">
+          <label htmlFor="datetime"> Fecha y hora de entrega : </label>
+          <input
+            className="form-control mt-2"
+            id="datetime"
+            name="datetime"
+            onChange={(e) => handleInputChange(e)}
+            type="datetime-local"
+            value={inputValue.datetime}
+          />
+        </div>
+
+        <div className="col">
+          <label htmlFor="month"> Mes: </label>
+          <input
+            className="form-control mt-2"
+            id="month"
+            name="month"
+            onChange={(e) => handleInputChange(e)}
+            type="month"
+            value={inputValue.month}
+          />
+        </div>
+
+        <div className="col">
+          <label htmlFor="time"> Hora : </label>
+          <input
+            className="form-control mt-2"
+            id="time"
+            name="time"
+            onChange={(e) => handleInputChange(e)}
+            type="time"
+            value={inputValue.time}
+          />
+        </div>
+
+        <div className="col">
+          <label htmlFor="week"> Semana: </label>
+          <input
+            className="form-control mt-2"
+            id="week"
+            name="week"
+            onChange={(e) => handleInputChange(e)}
+            type="week"
+            value={inputValue.week}
+          />
+        </div>
+
+        <div className="col">
+          <label htmlFor="file"> Seleccionar documentos : </label>
+          <input
+            className="form-control mt-2"
+            id="file"
+            name="file"
+            onChange={(e) => handleInputChange(e)}
+            type="file"
+            value={inputValue.file}
+          />
+        </div>
+      </div>
+
+      <div className="inputs-row4 row mt-5">
+        <div className="col">
+          <label htmlFor="reset"> Restablecer valores: </label>
+          <input
+            id="reset"
+            name="reset"
+            onChange={(e) => handleInputChange(e)}
+            type="reset"
+            value={inputValue.reset}
+          />
+        </div>
+
+        <div className="col">
+          <label htmlFor="hidden"> ID_CLIENTE: </label>
+          <input
+            className="form-control mt-2"
+            id="hidden"
+            name="hidden"
+            onChange={(e) => handleInputChange(e)}
+            type="hidden"
+            value={inputValue.hidden}
+          />
+        </div>
+
+        <div className="col">
+          <label htmlFor="image"> Seleccionar imagenes : </label>
+          <input
+            id="image"
+            name="image"
+            onChange={(e) => handleInputChange(e)}
+            type="image"
+            value={inputValue.image}
+          />
+        </div>
+
+        <div className="col">
+          <label htmlFor="radio"> Terminos y condiciones: </label>
+          <input
+            id="radio"
+            name="radio"
+            onChange={(e) => handleInputChange(e)}
+            type="radio"
+            value={inputValue.radio}
+          />
+        </div>
+
+        <div className="col">
+          <label htmlFor="search"> Buscar : </label>
+          <input
+            className="form-control mt-2"
+            id="search"
+            name="search"
+            onChange={(e) => handleInputChange(e)}
+            type="search"
+            value={inputValue.search}
+          />
+        </div>
+
+        <div className="col">
+          <label htmlFor="url"> Buscar URL : </label>
+          <input
+            className="form-control mt-2"
+            id="url"
+            name="url"
+            onChange={(e) => handleInputChange(e)}
+            type="url"
+            value={inputValue.url}
+          />
+        </div>
+      </div>
+
     </div>
   )
 }
